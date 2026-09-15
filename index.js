7ecd8981f19c368d9167ff878c22c9438d03d290
patiowalter3-dot <patiowalter3@gmail.com>
2026-09-15 11:58:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const TelegramBot = require('node-telegram-bot-api');
 const cron = require('node-cron');
 const fs = require('fs');
-
+const axios = require('axios');
 const app = express();
 app.use(express.json());
 
